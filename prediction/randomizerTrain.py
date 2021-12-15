357ba25d49dfeedc8e7d363d294cd2ccb2557c68
tinopai <eltinopa@icloud.com>
2021-12-15 18:23:46 -0300

    feat: Better print with emojis & simple control

--- prediction/randomizerTrain.py
+++ prediction/randomizerTrain.py
@@ -72,28 +72,35 @@ simple_pred = [1,2,2,3,3,4,5,5,6,6,7,7,8,8,9,10,10,11,11,12,12,13,13,14,15,15,16
 
 # unknown pred is the last prediction, it is human clicking and should return 'HUMAN' if the algo
 # is accurate enough
-unknown_pred = [2,3,4,5,7,7,9,10,12,13,14,15,17,18,19,20,22,23,24,26,27,28,30,31,31,33,34,35,37,38,39,41,42,43,45,46,47,49,50,51,52,54,55,57,58,59,60,62,63,64,66,67,68,69,70,70,70,71,72,72,74,75,77,78,79,80,82,83,84,86,87,88,90,91,92,94,95,95,96,97,98,99,100,101,103,104,106,107,107,108,109,111,112]
+human_control = [2,3,4,5,7,7,9,10,12,13,14,15,17,18,19,20,22,23,24,26,27,28,30,31,31,33,34,35,37,38,39,41,42,43,45,46,47,49,50,51,52,54,55,57,58,59,60,62,63,64,66,67,68,69,70,70,70,71,72,72,74,75,77,78,79,80,82,83,84,86,87,88,90,91,92,94,95,95,96,97,98,99,100,101,103,104,106,107,107,108,109,111,112]
+simple_control = [1,2,3,4,5,6,7,8,8,9,10,11,12,13,14,14,15,16,17,18,19,20,21,21,22,23,24,25,26,27,28,28,29,30,31,32,33,34,35,35,36,37,38,39,40,41,42,42,43,44,45,46,47,48,49,49,50,51,52,53,54,55,56,56,57,58,59,60,61,62,63,63,64,65,66,67,68,69,70,70,71,72,73,74,75,76,77,77,78,79]
 
 human_avg   = predict(human_pred)
 simple_avg  = predict(simple_pred)
-control_avg = predict(unknown_pred)
+simple_control_avg = predict(simple_control)
+human_control_avg  = predict(human_control)
 
+print('\n\n--🔗--    Values for debugging    --🔗--')
 print('Human: ', human_avg)
 print('Simple: ', simple_avg)
-print('Control: ', control_avg)
+avg = human_avg - simple_avg
+print('Average: ', avg)
+print('\nSimple control: ', simple_control_avg)
+print('Human control: ', human_control_avg)
 
 # simple should always be lower than human
 # if it isnt, the algo isnt being accurate enough
 if simple_avg > human_avg:
     print('Algorithm is inaccurate, simple should be lower than human.')
-    print('Simple: ', simple_avg)
-    print('Human: ', human_avg)
 
-avg = human_avg - simple_avg
-isControlHuman = control_avg > avg/2
-print('--- [ predict control ] ---')
-print('Average: ', avg)
+print('\n\n--🎲--    Control Results    --🎲--')
 
+# if (Control Average - Average) > Average then it's human, otherwise its not human
+isControlHuman = human_control_avg-avg > avg
+isControlSimple = simple_control_avg-avg > avg
+
+isAccurate = isControlHuman == True and isControlSimple != True
 # Should return true
-print('Control is human: ', isControlHuman)
-print(f'\nAlgorithm is {"accurate" if isControlHuman else "not accurate"}')
+print('🧠 Human Control is human: ', isControlHuman)
+print('🤖 Simple Control is human: ', isControlSimple)
+print(f'\n{"✔️" if isAccurate else "❌"}  Algorithm is {"accurate" if isAccurate else "not accurate"}')
