1c63358962a211b8cb5a46de1d9ec2a26c280069
tinopai <eltinopa@icloud.com>
2021-12-15 17:03:07 -0300

    feat: load only .txt files

--- prediction/dataToCSV.py
+++ prediction/dataToCSV.py
@@ -6,13 +6,14 @@ clicks = []
 
 for clickType in os.listdir('../clicks/'):
     for file in os.listdir('../clicks/' + clickType):
-        with open('../clicks/' + clickType + '/' + file, 'r') as file:
-            data = ''
-            for click in json.loads(file.read()):
-                data += str(click) + '|'
+        if file.endswith('.txt'):
+            with open('../clicks/' + clickType + '/' + file, 'r') as file:
+                data = ''
+                for click in json.loads(file.read()):
+                    data += str(click) + '|'
 
-            clicks.append(f'{data},{clickType}')
-            print(f'Appended {file.name}')
+                clicks.append(f'{data},{clickType}')
+                print(f'Appended {file.name}')
 
 with open('data.csv', 'w', newline = '') as csvfile:
     writer = csv.writer(csvfile, delimiter = '\n')
